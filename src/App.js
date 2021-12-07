de35d18db7ddc08db87bd34f204f3aeedc047071
Alexander Rousseeuw <alexander_rousseeuw@hotmail.com>
2021-12-07 20:23:31 +0100

    Add game to React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <canvas id="about" width="1000px" height="50px"/>
+        <br/>
+        <canvas id="stage" width="1000px" height="600px"/>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            <button id="start">Starta</button>
+            <button id="paus">Pausa</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
